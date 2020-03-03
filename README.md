d99c2ab10bb2d0c8edcee40325d902c6c118a2bd
charlesmartinreed <charlesmartinreed@icloud.com>
2020-03-02 19:00:29 -0600

    Add resize class method for scaling table size as table length grows.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 - The basic thing to undrestand is that we take the key, hash it and then use that has as a integer key for our created hash table.
+- Collision handling is implemented here as an example of how to handle issues with small table spaces.
--- hashTable.js
+++ hashTable.js
@@ -1,3 +1,5 @@
+//   we create a hash function to convert a key into a number
+
 function hashStringToInt(s, tableSize) {
   // prime numbers are preferably because they spready easily - both 17 and 13 are arbitrarily chosen, but the usage of primes in these positions is common
   let hash = 17;
@@ -13,17 +15,48 @@ function hashStringToInt(s, tableSize) {
 class HashTable {
   table = new Array(3);
 
-  //   we create a hash function to convert a key into a number
+  //   commonly, the closer you get to 1 with your loadfactor, the more likely you are to need to increase your table size
+  numItems = 0;
+
+  //   resize triggered when table is 80% full, minimum
+  resize = () => {
+    const newTable = new Array(this.table.length * 2);
+
+    // rehash the items
+    this.table.forEach(item => {
+      if (item) {
+        item.forEach(([key, value]) => {
+          const idx = hashStringToInt(key, newTable.length);
+          if (newTable[idx]) {
+            newTable[idx].push([key, value]);
+          } else {
+            newTable[idx] = [[key, value]];
+          }
+        });
+      }
+    });
+
+    this.table = newTable;
+  };
 
   setItem = (key, value) => {
+    this.numItems++;
     // e.g person[firstName] = 'bob'
     const idx = hashStringToInt(key, this.table.length);
 
-    // if already value at position...
+    const loadFactor = this.numItems / this.table.length;
+
+    if (loadFactor > 0.8) {
+      // here is where you'd resize
+      this.resize();
+    }
+
     if (this.table[idx]) {
+      // if already value at position...
       this.table[idx].push([key, value]);
     } else {
       // we store as an array of key/value pairs to correct against collisions from our table not being appropriately sized
+
       this.table[idx] = [[key, value]];
     }
   };
@@ -44,7 +77,9 @@ myTable.setItem("firstName", "bob");
 myTable.setItem("lastName", "jones");
 myTable.setItem("age", 32);
 myTable.setItem("dob", "3/5/87");
-console.log(myTable.table);
+myTable.setItem("location", "Chicago");
+
+console.log(myTable.table.length);
 
 console.log(myTable.getItem("firstName"));
 console.log(myTable.getItem("lastName"));
